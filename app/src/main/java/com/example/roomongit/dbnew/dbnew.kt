607cbb9012ef35bef4ed2d3da15ec6774bbe774a
Mykola Paziuk <external.Mykola.Paziuk@se.bosch.com>
2024-02-14 12:04:04 +0200

    creade new data class as database dbnew

---
+++ app/src/main/java/com/example/roomongit/dbnew/dbnew.kt
@@ -0,0 +1,32 @@
+package com.example.roomongit.dbnew
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+@Entity
+data class Todo (@PrimaryKey(autoGenerate = true) val id:Int? = null, val title:String, val note: String, val date: String )
+
+@Dao
+interface TodoDao {
+
+    @Insert
+    fun add(todo: Todo)
+
+    @Delete
+    fun delete(todo: Todo)
+
+    @Query("SELECT * FROM todo")
+    fun getAll(): LiveData<List<Todo>>
+}
+
+@Database(entities = [Todo::class], version = 1)
+abstract class TodoDatabase: RoomDatabase(){
+    abstract fun todoDao():TodoDao
+}
