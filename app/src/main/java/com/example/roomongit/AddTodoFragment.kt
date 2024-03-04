d7c16bf9e4071c6381bb99bcf6fcd25217f6dbeb
pzkmykola <mykola.pazuk@gmail.com>
2024-03-04 07:10:25 +0200

    updated add_todo_fragment with binding

--- app/src/main/java/com/example/roomongit/AddTodoFragment.kt
+++ app/src/main/java/com/example/roomongit/AddTodoFragment.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.roomongit.databinding.InputFragmentLayoutBinding
+import com.example.roomongit.dbnew.TodoFB
 
-class AddTodoFragment: Fragment() {
+class AddTodoFragment(private val todo: TodoFB? = null): Fragment() {
     private lateinit var viewModel: TodoViewModel
-
+    private lateinit var binding:InputFragmentLayoutBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,14 +23,16 @@ class AddTodoFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[TodoViewModel::class.java]
-        val titleInputField:EditText = view.findViewById(R.id.titleInputField)
-        val noteInputField:EditText = view.findViewById(R.id.noteInputField)
-        val dateInputField:EditText = view.findViewById(R.id.dateInputField)
-        val addButton:Button = view.findViewById(R.id.addButton)
-        addButton.setOnClickListener {
-            val title = titleInputField.text.toString()
-            val note = noteInputField.text.toString()
-            val date = dateInputField.text.toString()
+        binding = InputFragmentLayoutBinding.bind(view)
+        todo?.let {
+            binding.titleInputField.setText(it.title)
+            binding.noteInputField.setText(it.note)
+            binding.dateInputField.setText(it.date)
+        }
+        binding.addButton.setOnClickListener {
+            val title = binding.titleInputField.text.toString()
+            val note = binding.noteInputField.text.toString()
+            val date = binding.dateInputField.text.toString()
             viewModel.addTodo(title, note, date)
             parentFragmentManager.popBackStack()
         }
